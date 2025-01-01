7c4e6723e8f10ae0be3216efcbac1898ef78684f
akbarryyan <akbarrayyan182@gmail.com>
2025-01-01 15:49:08 +0700

    Update Login

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -53,7 +53,7 @@
         <div class="auth-right py-40 px-24 flex-center flex-column">
             <div class="auth-right__inner mx-auto w-100">
                 <a href="index.html" class="auth-right__logo">
-                    <img src="assets/images/logo/logo.png" alt="">
+                    <img src="https://html.themeholy.com/edmate/assets/images/logo/logo.png" alt="">
                 </a>
                 <h2 class="mb-8">Welcome to Back! &#128075;</h2>
                 <p class="text-gray-600 text-15 mb-32">Please sign in to your admin account</p>
@@ -82,31 +82,6 @@
                         <a href="forgot-password.html" class="text-main-600 hover-text-decoration-underline text-15 fw-medium">Forgot Password?</a>
                     </div>
                     <button type="submit" class="btn btn-main rounded-pill w-100">Sign In</button>
-                    <p class="mt-32 text-gray-600 text-center">New on our platform?
-                        <a href="sign-up.html" class="text-main-600 hover-text-decoration-underline">Create an account</a>
-                    </p>
-
-                    <div class="divider my-32 position-relative text-center">
-                        <span class="divider__text text-gray-600 text-13 fw-medium px-26 bg-white">or</span>
-                    </div>
-
-                    <ul class="flex-align gap-10 flex-wrap justify-content-center">
-                        <li>
-                            <a href="https://www.facebook.com" class="w-38 h-38 flex-center rounded-6 text-facebook-600 bg-facebook-50 hover-bg-facebook-600 hover-text-white text-lg">
-                                <i class="ph-fill ph-facebook-logo"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.twitter.com" class="w-38 h-38 flex-center rounded-6 text-twitter-600 bg-twitter-50 hover-bg-twitter-600 hover-text-white text-lg">
-                                <i class="ph-fill ph-twitter-logo"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.google.com" class="w-38 h-38 flex-center rounded-6 text-google-600 bg-google-50 hover-bg-google-600 hover-text-white text-lg">
-                                <i class="ph ph-google-logo"></i>
-                            </a>
-                        </li>
-                    </ul>
                 </form>
                 <div id="message" class="mt-3"></div>
             </div>
