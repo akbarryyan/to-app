5a70a3290d403b75cc0bfcee9fc03dc0ce67357b
akbarryyan <akbarrayyan182@gmail.com>
2025-02-26 09:13:07 +0700

    Update morning

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tryout;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class AdminDashboardController extends Controller
@@ -10,11 +11,13 @@ class AdminDashboardController extends Controller
     public function index(Request $request)
     {
         $tryouts = Tryout::all();
+        $categories = Category::all();
+        
         if ($request->ajax()) {
-            $html = view('admin.partials.dashboard', compact('tryouts'))->render();
+            $html = view('admin.partials.dashboard', compact('tryouts', 'categories'))->render();
             return response()->json(['html' => $html], 200);
         }
 
-        return view('admin.dashboard', compact('tryouts'));
+        return view('admin.dashboard', compact('tryouts', 'categories'));
     }
 }
